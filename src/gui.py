775c5baacb270955426ca4c3c6bc980d53e691bf
FMWcatcat <nikomakela15@gmail.com>
2025-05-13 10:31:15 +0300

    more code

--- src/gui.py
+++ src/gui.py
@@ -10,33 +10,41 @@ def select_folder():
     path_var.set('')
 
 
-#--     --      --      --      --
-dr_sw_width = 800
-dr_sw_height = 500
+dr_w_width = 800
+dr_w_height = 500
 
 screen_width = root.winfo_screenwidth()
 screen_height = root.winfo_screenheight()
 
-x = (screen_width / 2) - (dr_sw_width / 2)
-y = (screen_height / 2) - (dr_sw_height / 2)
+x = (screen_width / 2) - (dr_w_width / 2)
+y = (screen_height / 2) - (dr_w_height / 2)
 
-root.geometry(f'{dr_sw_width}x{dr_sw_height}+{int(x)}+{int(y)}')
+root.geometry(f'{dr_w_width}x{dr_w_height}+{int(x)}+{int(y)}')
 root.title('Duplicate Remover')
-#--     --      --      --      --
 
 
 #def save_folder_path():
  #   for s in :
+#get([end-1c])
+def clear_text():
+    input_folder_entry.delete(0, "end")
 
+def enter_path():
+    input_folder_entry.
 
 input_folder_label = tk.Label(root, text = 'file_path', font=('calibri', 16))
-input_folder_intructions = tk.Label(root, text = 'Insert target folder filepath for it to be processed.', font=('calibri', 12))
-input_folder_entry = tk.Entry(root, textvariable = path_var, font=('calibri', 12))
-input_folder = tk.Button(root, text='Select folder path', width=15, command=root.destroy)
-
+input_folder_intructions = tk.Label(root, text = 'Insert target folder filepath.', font=('calibri', 12))
+input_folder_entry = tk.Entry(root, textvariable = path_var, width=40, font=('calibri', 12))
+input_folder_enter = tk.Button(root, text='Enter', width=15, command=enter_path)
+input_folder_clear = tk.Button(root, text='Clear', width=15, command=clear_text)
+#command=root.destroy
 input_folder_label.place(x=400, y=100, anchor=CENTER)
 input_folder_intructions.place(x=400, y=190, anchor=CENTER)
 input_folder_entry.place(x=400, y=130, anchor=CENTER)
-input_folder.place(x=400, y=160, anchor=CENTER)
+input_folder_enter.place(x=500, y=160, anchor=CENTER)
+input_folder_clear.place(x=300, y=160, anchor=CENTER)
+
+
+
 
 root.mainloop()

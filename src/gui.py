c163a87c825d6188f76f15074f8f62a5080a147e
FMWcatcat <nikomakela15@gmail.com>
2025-05-13 09:49:39 +0300

    centering

--- src/gui.py
+++ src/gui.py
@@ -7,19 +7,36 @@ path_var=tk.StringVar()
 
 def select_folder():
     path = path_var.get()
-    path_var.set("")
-
-root.geometry("800x500")
-root.title("Duplicate Remover")
-button = tk.Button(root, text="Select input folder", width=15, command=root.destroy)
+    path_var.set('')
 
 
+#--     --      --      --      --
+dr_sw_width = 800
+dr_sw_height = 500
 
-input_folder_label = tk.Label(root, text = "file_path", font=("calibri", 12))
-input_folder_entry = tk.Entry(root,textvariable = path_var, font=("calibri", 12))
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
 
-input_folder_label.grid(row=0,column=0)
-input_folder_entry.grid(row=1,column=0)
-button.grid(row=1,column=1)
+x = (screen_width / 2) - (dr_sw_width / 2)
+y = (screen_height / 2) - (dr_sw_height / 2)
+
+root.geometry(f'{dr_sw_width}x{dr_sw_height}+{int(x)}+{int(y)}')
+root.title('Duplicate Remover')
+#--     --      --      --      --
+
+
+#def save_folder_path():
+ #   for s in :
+
+
+input_folder_label = tk.Label(root, text = 'file_path', font=('calibri', 16))
+input_folder_intructions = tk.Label(root, text = 'Insert target folder filepath for it to be processed.', font=('calibri', 12))
+input_folder_entry = tk.Entry(root, textvariable = path_var, font=('calibri', 12))
+input_folder = tk.Button(root, text='Select folder path', width=15, command=root.destroy)
+
+input_folder_label.place(x=400, y=100, anchor=CENTER)
+input_folder_intructions.place(x=400, y=190, anchor=CENTER)
+input_folder_entry.place(x=400, y=130, anchor=CENTER)
+input_folder.place(x=400, y=160, anchor=CENTER)
 
 root.mainloop()
